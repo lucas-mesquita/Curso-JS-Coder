9053acbd99de43f532609654daf4807f50e01624
Lucas Mesquita <lucas.mes1011@gmail.com>
2023-05-03 00:00:17 -0300

    Modificaciones

--- main.js
+++ main.js
@@ -1,71 +1,73 @@
-let nombre = prompt(`Bienvenido al cotizador de cuadros\nPor favor, ingrese su nombre`);
-alert(`${nombre}, para realizar una cotizacion vamos\na solicitarte dos medidas en centímetros.`)
+// Definir constantes y variables globales
+const materiales = {
+  lienzo: { nombre: "Lienzo", precio: 2000, stock: 10 },
+  vidrio: { nombre: "Vidrio", precio: 2500, stock: 5 },
+  aluminio: { nombre: "Aluminio", precio: 3000, stock: 8 }
+};
 
-// Calcular m2
-const m2 = (a, b) => (a * b) / 10000;
+// Definir funciones
+const m2 = (ancho, alto, material, personalizado) => {
+  let precio = material.precio;
+  if (personalizado) {
+    precio += 1000; // agregar costo extra de diseño
+  }
+  return (ancho * alto * precio) / 10000;
+};
 
-// Costos de materiales
-const m2Lienzo = 100;
-const m2Vidrio = 150;
-const m2Aluminio = 200;
+const generarInfoPagos = (costo) => {
+  const contado = costo.toFixed(2);
+  const cuotas3 = (costo / 3).toFixed(2);
+  const cuotas6 = ((costo / 6) * 1.5).toFixed(2);
+  const cuotas12 = ((costo / 12) * 1.1).toFixed(2);
+  
+  return `Puede realizar el pago de las siguientes maneras:
+Contado $${contado}
+3 Cuotas $${cuotas3}
+6 Cuotas $${cuotas6}
+12 Cuotas $${cuotas12}
+Con esta información comunicarse con un operador.`;
+};
 
-// Ingreso de medidas
-let ancho = parseFloat(prompt('Ingrese el ancho a cotizar'));
-while (isNaN(ancho)) {
-  ancho = parseFloat(prompt('El valor ingresado no es válido. Ingrese el ancho a cotizar'));
+const obtenerMedida = (mensaje) => {
+  let medida = NaN;
+  while (isNaN(medida)) {
+    medida = parseFloat(prompt(mensaje));
+    if (isNaN(medida)) {
+      alert("El valor ingresado no es válido. Ingrese una medida en centímetros.");
+    }
+  }
+  return medida;
+};
+
+// Solicitar información al usuario
+let nombre = prompt("Bienvenido al cotizador de cuadros. Por favor, ingrese su nombre:");
+alert(`Hola, ${nombre}. Para realizar una cotización, vamos a solicitarle dos medidas en centímetros.`);
+console.log(nombre);
+const ancho = obtenerMedida("Ingrese el ancho a cotizar:");
+const alto = obtenerMedida("Ingrese el alto a cotizar:");
+
+let opcionCompra = NaN;
+const esPersonalizado = confirm("¿Desea personalizar su cuadro por un costo adicional de $1000?");
+while (isNaN(opcionCompra) || opcionCompra < 1 || opcionCompra > 3) {
+  opcionCompra = parseInt(prompt(`El costo del cuadro en los diferentes materiales son:
+  1- Lienzo $${materiales.lienzo.precio}
+  2- Vidrio $${materiales.vidrio.precio}
+  3- Aluminio $${materiales.aluminio.precio}
+  Por favor, ingrese un número correspondiente a uno de los materiales.`));
+  if (isNaN(opcionCompra) || opcionCompra < 1 || opcionCompra > 3) {
+    alert("El valor ingresado no es válido. Ingrese un número correspondiente a uno de los materiales.");
+  }
 }
-console.log(ancho);
-let alto = parseFloat(prompt('Ingrese el alto a cotizar'));
-while (isNaN(alto)) {
-  alto = parseFloat(prompt('El valor ingresado no es válido. Ingrese el alto a cotizar'));
-}
-console.log(alto);
+// Calcular costo de material
+const materialSeleccionado = materiales[Object.keys(materiales)[opcionCompra - 1]];
+const costoMaterial = m2(ancho, alto, materialSeleccionado);
 
-let costoLienzo = m2(ancho, alto) * m2Lienzo;
-let costoVidrio = m2(ancho, alto) * m2Vidrio;
-let costoAluminio = m2(ancho, alto) * m2Aluminio;
-
-let opcionCompra = Number(prompt(`El costo del cuadro en los diferentes materiales son:
-1-Lienzo $${costoLienzo}
-2-Vidrio $${costoVidrio}
-3-Aluminio $${costoAluminio}
-Por favor, ingrese un numero correspondiente a uno de los materiales.`));
-while (isNaN(opcionCompra)) {
-  opcionCompra = Number(prompt('Por favor, ingrese un numero correspondiente a uno de los materiales.'));
+// Verifica si el material seleccionado esta en stock
+if (materialSeleccionado.stock <= 0) {
+  alert(`Lo sentimos, el material ${materialSeleccionado.nombre} no está disponible en este momento.`);
+  return;
 }
 
-switch (opcionCompra) {
-  case 1:
-    alert(`El material seleccionado es Lienzo
-    Puede realizar el pago de las siguientes maneras.
-    Contado $${costoLienzo}
-    3 Cuotas $${costoLienzo / 3}
-    6 Cuotas $${(costoLienzo / 6) * 0.5}
-    12 Cuotas $${(costoLienzo / 12) * 0.1}
-    Con esta información comunicarse con un operador.`)
-    break;
-
-  case 2:
-    alert(`El material seleccionado es Vidrio
-    Puede realizar el pago de las siguientes maneras.
-    Contado $${costoVidrio}
-    3 Cuotas $${costoVidrio / 3}
-    6 Cuotas $${(costoVidrio / 6) * 0.5}
-    12 Cuotas $${(costoVidrio / 12) * 0.1}
-    Con esta información comunicarse con un operador.`)
-    break;
-
-  case 3:
-    alert(`El material seleccionado es Aluminio
-    Puede realizar el pago de las siguientes maneras.
-    Contado $${costoAluminio}
-    3 Cuotas $${costoAluminio / 3}
-    6 Cuotas $${(costoAluminio / 6) * 0.5}
-    12 Cuotas $${(costoAluminio / 12) * 0.1}
-    Con esta información comunicarse con un operador.`)
-    break;
-  default:
-    opcionCompra = Number(prompt('Por favor, ingrese un numero correspondiente a uno de los materiales.'))
-    break;
-
-}
+// Mostrar información al usuario
+const infoPagos = generarInfoPagos(costoMaterial);
+alert(`El material seleccionado es ${materialSeleccionado.nombre}.\n${infoPagos}`);
